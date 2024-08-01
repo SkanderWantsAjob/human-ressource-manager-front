f40642b7ad12384d58fa66f2205fc1196e931dab
skander <benachour.skander1@gmail.com>
2024-08-01 11:01:29 +0100

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { encapsulateStyle } from '@angular/compiler';
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+
+})
+export class HomeComponent {}
